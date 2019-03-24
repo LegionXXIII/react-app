bace8b42603721448b6fbe31da8bcef29d4f13b5
Traian <traianmihali696@gmail.com>
2019-03-25 01:14:09 +0200

    changed raven-js with the latest sentry sdk, @sentry/browser

--- src/services/logService.js
+++ src/services/logService.js
@@ -1,15 +1,22 @@
-import Raven from "raven-js";
+// import Raven from "raven-js";
+import * as Sentry from "@sentry/browser";
+
+// function init() {
+//   Raven.config("https://3edfdb68f8f946249679b3067ab785dd@sentry.io/1422385", {
+//     release: "1-0-0",
+//     environment: "development-test"
+//   }).install();
+// }
 
 function init() {
-  Raven.config("https://3edfdb68f8f946249679b3067ab785dd@sentry.io/1422385", {
-    release: "1-0-0",
-    environment: "development-test"
-  }).install();
+  Sentry.init({
+    dsn: "https://89cb5c1c76134627a2848b9fb84cfeb8@sentry.io/1422653",
+    release: "react-app@1.0.0"
+  });
 }
 
 function log(error) {
-  Raven.captureException(error);
-  console.log(error);
+  Sentry.captureException(error);
 }
 
 export default {
