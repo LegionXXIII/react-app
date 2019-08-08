f8a1a6cee86dfec273f2eda0abecc44088580519
Traian <traianmihali696@gmail.com>
2019-08-08 14:04:55 +0300

    fix user detection by using this.props.user

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -43,7 +43,7 @@ class NavBar extends Component {
             <NavLink to="/rentals" className="nav-item nav-link">
               Rentals
             </NavLink>
-            {!this.user && (
+            {!this.props.user && (
               <React.Fragment>
                 <NavLink to="/login" className="nav-item nav-link">
                   Login
@@ -53,10 +53,10 @@ class NavBar extends Component {
                 </NavLink>
               </React.Fragment>
             )}
-            {this.user && (
+            {this.props.user && (
               <React.Fragment>
                 <NavLink to="/profile" className="nav-item nav-link">
-                  {this.user.name}
+                  {this.props.user.name}
                 </NavLink>
                 <NavLink to="/logout" className="nav-item nav-link">
                   Logout
@@ -71,59 +71,3 @@ class NavBar extends Component {
 }
 
 export default NavBar;
-
-// const NavBar = ({ user }) => {
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//       <Link to="/" className="navbar-brand">
-//         Vidly
-//       </Link>
-//       <button
-//         className="navbar-toggler"
-//         type="button"
-//         data-toggle="collapse"
-//         data-target="#navbarNavAltMarkup"
-//         aria-controls="navbarNavAltMarkup"
-//         aria-expanded="false"
-//         aria-label="Toggle navigation"
-//       >
-//         <span className="navbar-toggler-icon" />
-//       </button>
-//       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-//         <div className="navbar-nav">
-//           <NavLink to="/movies" className="nav-item nav-link">
-//             Movies
-//           </NavLink>
-//           <NavLink to="/customers" className="nav-item nav-link">
-//             Customers
-//           </NavLink>
-//           <NavLink to="/rentals" className="nav-item nav-link">
-//             Rentals
-//           </NavLink>
-//           {!user && (
-//             <React.Fragment>
-//               <NavLink to="/login" className="nav-item nav-link">
-//                 Login
-//               </NavLink>
-//               <NavLink to="/register" className="nav-item nav-link">
-//                 Register
-//               </NavLink>
-//             </React.Fragment>
-//           )}
-//           {user && (
-//             <React.Fragment>
-//               <NavLink to="/profile" className="nav-item nav-link">
-//                 {user.name}
-//               </NavLink>
-//               <NavLink to="/logout" className="nav-item nav-link">
-//                 Logout
-//               </NavLink>
-//             </React.Fragment>
-//           )}
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default NavBar;
