17094c82380e18d715159336e117bdac3aa51b9e
Traian <traianmihali696@gmail.com>
2019-08-08 13:54:27 +0300

    add support for responsive navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,58 +1,129 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
 
-const NavBar = ({ user }) => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link to="/" className="navbar-brand">
-        Vidly
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <NavLink to="/movies" className="nav-item nav-link">
-            Movies
-          </NavLink>
-          <NavLink to="/customers" className="nav-item nav-link">
-            Customers
-          </NavLink>
-          <NavLink to="/rentals" className="nav-item nav-link">
-            Rentals
-          </NavLink>
-          {!user && (
-            <React.Fragment>
-              <NavLink to="/login" className="nav-item nav-link">
-                Login
-              </NavLink>
-              <NavLink to="/register" className="nav-item nav-link">
-                Register
-              </NavLink>
-            </React.Fragment>
-          )}
-          {user && (
-            <React.Fragment>
-              <NavLink to="/profile" className="nav-item nav-link">
-                {user.name}
-              </NavLink>
-              <NavLink to="/logout" className="nav-item nav-link">
-                Logout
-              </NavLink>
-            </React.Fragment>
-          )}
+class NavBar extends Component {
+  state = {
+    expanded: false
+  };
+
+  handleClick = () => {
+    let expanded = !this.state.expanded;
+    this.setState({ expanded });
+  };
+
+  render() {
+    let className = "collapse navbar-collapse";
+    if (this.state.expanded) className += " show";
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" className="navbar-brand">
+          Vidly
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={this.handleClick}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={className} id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink to="/movies" className="nav-item nav-link">
+              Movies
+            </NavLink>
+            <NavLink to="/customers" className="nav-item nav-link">
+              Customers
+            </NavLink>
+            <NavLink to="/rentals" className="nav-item nav-link">
+              Rentals
+            </NavLink>
+            {!this.user && (
+              <React.Fragment>
+                <NavLink to="/login" className="nav-item nav-link">
+                  Login
+                </NavLink>
+                <NavLink to="/register" className="nav-item nav-link">
+                  Register
+                </NavLink>
+              </React.Fragment>
+            )}
+            {this.user && (
+              <React.Fragment>
+                <NavLink to="/profile" className="nav-item nav-link">
+                  {this.user.name}
+                </NavLink>
+                <NavLink to="/logout" className="nav-item nav-link">
+                  Logout
+                </NavLink>
+              </React.Fragment>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  }
+}
 
 export default NavBar;
+
+// const NavBar = ({ user }) => {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//       <Link to="/" className="navbar-brand">
+//         Vidly
+//       </Link>
+//       <button
+//         className="navbar-toggler"
+//         type="button"
+//         data-toggle="collapse"
+//         data-target="#navbarNavAltMarkup"
+//         aria-controls="navbarNavAltMarkup"
+//         aria-expanded="false"
+//         aria-label="Toggle navigation"
+//       >
+//         <span className="navbar-toggler-icon" />
+//       </button>
+//       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+//         <div className="navbar-nav">
+//           <NavLink to="/movies" className="nav-item nav-link">
+//             Movies
+//           </NavLink>
+//           <NavLink to="/customers" className="nav-item nav-link">
+//             Customers
+//           </NavLink>
+//           <NavLink to="/rentals" className="nav-item nav-link">
+//             Rentals
+//           </NavLink>
+//           {!user && (
+//             <React.Fragment>
+//               <NavLink to="/login" className="nav-item nav-link">
+//                 Login
+//               </NavLink>
+//               <NavLink to="/register" className="nav-item nav-link">
+//                 Register
+//               </NavLink>
+//             </React.Fragment>
+//           )}
+//           {user && (
+//             <React.Fragment>
+//               <NavLink to="/profile" className="nav-item nav-link">
+//                 {user.name}
+//               </NavLink>
+//               <NavLink to="/logout" className="nav-item nav-link">
+//                 Logout
+//               </NavLink>
+//             </React.Fragment>
+//           )}
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default NavBar;
